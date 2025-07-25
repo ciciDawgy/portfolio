315bcb5ca0e4c0a046f20412f406c022d3e2f9a4
ciciDawgy <108370999+ciciDawgy@users.noreply.github.com>
2025-07-25 11:38:42 -0400

    Add files via upload

---
+++ slideRev/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    $('.next').on('click', function() {
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if(nextImg.length){
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+    
+    $('.prev').on('click', function(){
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+
+        if(prevImg.length){
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
+    });
+
+    $("button").click(function(){
+        $("p").toggle();
+      });
+  })
+
+
+
+  /*
+
+$(function() {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
+*/
